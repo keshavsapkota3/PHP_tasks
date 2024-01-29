70c573a0713e7d25499a77585d7bda4c58bde42b
keshavsapkota3 <109505297+keshavsapkota3@users.noreply.github.com>
2024-01-29 06:53:15 +0200

    helo hello

--- hello.php
+++ hello.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercise 1: Getting Started with PHP - Nitish Raj</title>
+    <title>Exercise 1: Getting Started with PHP - Keshav Sapkota</title>
 </head>
 <body>
     <h3>Write PHP code to output the following message:
